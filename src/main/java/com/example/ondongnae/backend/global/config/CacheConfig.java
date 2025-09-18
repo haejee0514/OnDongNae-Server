2db76fc8ad178bd6e070448796337c710f8ef5f6
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-09-18 17:23:09 +0900

    feat: CacheConfig 추가

---
+++ src/main/java/com/example/ondongnae/backend/global/config/CacheConfig.java
@@ -0,0 +1,33 @@
+package com.example.ondongnae.backend.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        var storeDetail = new CaffeineCache(
+                "store-detail", // 캐시 이름
+                Caffeine.newBuilder()
+                        .expireAfterWrite(Duration.ofSeconds(60)) // TTL 60초
+                        .maximumSize(500) // 최대 엔트리
+                        .recordStats() // 통계 수집
+                        .build()
+        );
+
+        var mgr = new SimpleCacheManager();
+        mgr.setCaches(List.of(storeDetail));
+        return mgr;
+    }
+}
